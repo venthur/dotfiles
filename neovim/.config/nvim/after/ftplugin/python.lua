60fa0f202bd68ccee97f9898d28b913bb8f82061
Bastian Venthur <venthur@debian.org>
2025-09-26 09:43:34 +0200

    fixed python ident, added shortcut for spellcheck

--- neovim/.config/nvim/after/ftplugin/python.lua
+++ neovim/.config/nvim/after/ftplugin/python.lua
@@ -1,5 +1,7 @@
 vim.bo.textwidth=79
 -- help ft-python-indent
-vim.g.pyindent_open_paren = 'shiftwidth()'
-vim.g.pyindent_continue = 'shiftwidth()'
--- let g:python_indent.closed_paren_align_last_line = v:false
+vim.g.python_indent = {
+  open_paren = 'shiftwidth()',
+  continue = 'shiftwidth()',
+  closed_paren_align_last_line = false,
+}
